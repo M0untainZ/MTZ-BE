564af76393d320bde921fe56560cf23efce28129
vuddus526 <vuddus526@naver.com>
2022-11-06 15:23:54 +0900

    🎉Begin: init project

---
+++ src/main/java/MTZ/mountainz/MountainzApplication.java
@@ -0,0 +1,13 @@
+package MTZ.mountainz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountainzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MTZ/mountainz/MountainzApplicationTests.java
@@ -0,0 +1,13 @@
+package MTZ.mountainz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountainzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
