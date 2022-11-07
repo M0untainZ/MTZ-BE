5b58f916f5e13bf402048c3f96f9155ccdf347d2
pdongjaelee <kyp4810@gmail.com>
2022-11-07 15:22:37 +0900

    ⭐Feat : add JwtAuthFilter, JwtUtil, RefreshToken, RefreshTokenRepository, TokenDto

---
+++ src/main/java/MTZ/mountainz/domain/member/repository/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package MTZ.mountainz.domain.member.repository;
+
+import MTZ.mountainz.domain.member.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserEmail(String email);
+}
