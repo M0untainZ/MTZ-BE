aeb25fdb30f08bac960e797f4a16a3462834de8b
pdongjaelee <kyp4810@gmail.com>
2022-11-07 19:30:21 +0900

    🐛Fix : yml and MemberEmail -> UserEmail

--- src/main/java/MTZ/mountainz/domain/member/repository/RefreshTokenRepository.java
+++ src/main/java/MTZ/mountainz/domain/member/repository/RefreshTokenRepository.java
@@ -2,8 +2,11 @@ package MTZ.mountainz.domain.member.repository;
 
 import MTZ.mountainz.domain.member.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    Optional<RefreshToken> findByMemberEmail(String email);
+    Optional<RefreshToken> findByUserEmail(String email);
 }
