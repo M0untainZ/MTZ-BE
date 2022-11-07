64cc092397bf4bf9dbcd3c40444054824f24d235
vuddus526 <vuddus526@naver.com>
2022-11-07 15:55:48 +0900

    ⭐Feat : add cors

--- src/main/java/MTZ/mountainz/global/security/WebSecurityConfig.java
+++ src/main/java/MTZ/mountainz/global/security/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration // 설정파일을 만들기 위한 어노테이션 or Bean을 등록하기 위한 어노테이션
@@ -43,6 +44,8 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+//        http.cors().configurationSource(corsConfigurationSource());
+
         // CORS
         http.cors().configurationSource(request -> {
             var cors = new CorsConfiguration();
@@ -70,4 +73,38 @@ public class WebSecurityConfig {
 
         return http.build();
     }
+
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//
+//         /*헤더에 작성된 출처만 브라우저가 리소스를 접근할 수 있도록 허용함.
+//        Access-Control-Allow-Origin: "https://naver.com"
+//        리소스 접근을 허용하는 HTTP 메서드를 지정해 주는 헤더
+//        Access-Control-Request-Methods: GET, POST, PUT, DELETE
+//        서버에서 응답 헤더에 추가해 줘야 브라우저의 자바스크립트에서 헤더에 접근 허용
+//        Access-Control-Expose-Headers: Authorization
+//        preflight 요청 결과를 캐시 할 수 있는 시간을 나타냄.
+//        60초 동안 preflight 요청을 캐시하는 설정으로, 첫 요청 이후 60초 동안은 OPTIONS 메소드를 사용하는 예비 요청을 보내지 않는다.
+//        Access-Control-Max-Age: 60
+//        자바스크립트 요청에서 credentials가 include일 때 요청에 대한 응답을 할 수 있는지를 나타낸다
+//        Access-Control-Allow-Credentials: true*/
+//
+//
+//        //프론트쪽 URL기재 필수!
+//        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+//
+//        configuration.setAllowedMethods(Arrays.asList("*"));
+//
+//        configuration.setAllowedHeaders(Arrays.asList("*"));
+//
+//        configuration.setExposedHeaders(Arrays.asList("Authorization", "Refresh_Token"));
+//
+//        configuration.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//
+//        return source;
+//    }
 }
