204ed11a6089db92ad719f2dc571c93bf101799f
vuddus526 <vuddus526@naver.com>
2022-11-09 19:52:34 +0900

    ⭐Feat : add certification

---
+++ src/main/java/MTZ/mountainz/domain/certification/controller/CertificationController.java
@@ -0,0 +1,30 @@
+package MTZ.mountainz.domain.certification.controller;
+
+import MTZ.mountainz.domain.certification.dto.request.CertificationRequestDto;
+import MTZ.mountainz.domain.certification.entity.Certification;
+import MTZ.mountainz.domain.certification.service.CertificationService;
+import MTZ.mountainz.global.dto.ResponseDto;
+import MTZ.mountainz.global.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class CertificationController {
+    private final CertificationService certificationService;
+
+    // 인증 페이지에 인증사진 불러오기
+    @GetMapping("/photoList")
+    public ResponseDto<?> certificationList (@AuthenticationPrincipal UserDetailsImpl userDetailsImpl){
+        return certificationService.certificationList(userDetailsImpl.getMember().getEmail());
+    }
+
+    // 인증사진 삭제하기
+    @DeleteMapping("/photoList/sakje")
+    public ResponseDto<?> certificationSakje (@RequestBody CertificationRequestDto certificationRequestDto) {
+        return certificationService.certificationSakje(certificationRequestDto);
+    }
+
+}
---
+++ src/main/java/MTZ/mountainz/domain/certification/service/CertificationService.java
@@ -0,0 +1,41 @@
+package MTZ.mountainz.domain.certification.service;
+
+import MTZ.mountainz.domain.certification.dto.request.CertificationRequestDto;
+import MTZ.mountainz.domain.certification.entity.Certification;
+import MTZ.mountainz.domain.certification.repository.CertificationRepositoy;
+import MTZ.mountainz.global.dto.ResponseDto;
+import com.amazonaws.services.s3.AmazonS3Client;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CertificationService {
+    private final CertificationRepositoy certificationRepositoy;
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    // 인증 페이지에 인증사진 불러오기
+    public ResponseDto<?> certificationList(String email) {
+        // cetification 테이블 정보 다 불러오기
+        List<Certification> certificationList = certificationRepositoy.findAll();
+
+        return ResponseDto.success(certificationList);
+    }
+
+    // 인증사진 삭제하기
+    public ResponseDto<?> certificationSakje(CertificationRequestDto certificationRequestDto) {
+        String[] split = certificationRequestDto.getPhoto().split("/");
+        String key = split[split.length-1].trim();
+
+        amazonS3Client.deleteObject(bucketName, key);
+
+        return ResponseDto.success(key + "를 삭제 완료 했습니다");
+    }
+}
