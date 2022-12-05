36d9e7b4ef9a99c5b435a7e2b387722a014e1152
vuddus526 <vuddus526@naver.com>
2022-12-05 23:34:49 +0900

    🔨Refactor : update memberBadge

--- src/main/java/MTZ/mountainz/domain/badge/entity/MemberBadge.java
+++ src/main/java/MTZ/mountainz/domain/badge/entity/MemberBadge.java
@@ -1,17 +1,25 @@
 package MTZ.mountainz.domain.badge.entity;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import MTZ.mountainz.domain.member.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Entity
 @Getter
@@ -19,30 +27,28 @@ import java.time.format.DateTimeFormatter;
 @AllArgsConstructor
 public class MemberBadge {
 
-    LocalDateTime date = LocalDateTime.now();
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "memberBadge_id", nullable = false)
+	private Long id;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "memberBadge_id", nullable = false)
-    private Long id;
+	@JoinColumn(name = "badge_id", nullable = false)
+	@ManyToOne(fetch = FetchType.LAZY)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Badge badge;
 
-    @JoinColumn(name = "badge_id", nullable = false)
-    @ManyToOne(fetch = FetchType.LAZY)
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    private Badge badge;
+	@JoinColumn(name = "member_id", nullable = false)
+	@ManyToOne(fetch = FetchType.LAZY)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Member member;
 
+	@DateTimeFormat
+	private String openTime; // 뱃지 획득 시간
 
-    @JoinColumn(name = "member_id", nullable = false)
-    @ManyToOne(fetch = FetchType.LAZY)
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    private Member member;
-
-    @DateTimeFormat
-    private String openTime; // 뱃지 획득 시간
-
-    public void createDate(Badge badge, Member member) {
-        this.openTime = date.format(DateTimeFormatter.ISO_DATE);
-        this.badge = badge;
-        this.member = member;
-    }
+	public void createDate(Badge badge, Member member) {
+		LocalDateTime date = LocalDateTime.now();
+		this.openTime = date.format(DateTimeFormatter.ISO_DATE);
+		this.badge = badge;
+		this.member = member;
+	}
 }
