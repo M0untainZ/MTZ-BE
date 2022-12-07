f294d1c55bdd66dae2a93816071edb8252060b2c
vuddus526 <vuddus526@naver.com>
2022-12-07 18:03:07 +0900

    ⭐Feat : add page direction

--- src/main/java/MTZ/mountainz/domain/certification/controller/CertificationController.java
+++ src/main/java/MTZ/mountainz/domain/certification/controller/CertificationController.java
@@ -1,16 +1,22 @@
 package MTZ.mountainz.domain.certification.controller;
 
-import io.swagger.annotations.ApiOperation;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import MTZ.mountainz.domain.certification.dto.request.CertificationRequestDto;
 import MTZ.mountainz.domain.certification.dto.request.PhotoFilterRequestDto;
 import MTZ.mountainz.domain.certification.service.CertificationService;
 import MTZ.mountainz.global.dto.ResponseDto;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -22,7 +28,9 @@ public class CertificationController {
 	// 인증 페이지에 인증사진 불러오기
 	@GetMapping("/photos")
 	@ApiOperation(value = "인증 페이지에 인증사진 불러오기", notes = "인증 페이지에 인증사진 불러오기 API")
-	public ResponseDto<?> certificationList(@PageableDefault(size = 12) Pageable pageable, @AuthenticationPrincipal UserDetails userDetails) {
+	public ResponseDto<?> certificationList(
+		@PageableDefault(size = 12, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+		@AuthenticationPrincipal UserDetails userDetails) {
 		return certificationService.certificationList(userDetails.getUsername(), pageable);
 	}
 
